8b21cf58996aa2d314ce11d6b2cc90d67b9a6162
earth774 <sutthiphongnuanma@gmail.com>
2022-04-30 10:10:09 +0700

    create component first

--- src/app/action-bar/action-bar.component.ts
+++ src/app/action-bar/action-bar.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-action-bar',
   templateUrl: './action-bar.component.html',
-  styleUrls: ['./action-bar.component.scss']
+  styleUrls: ['./action-bar.component.scss'],
 })
 export class ActionBarComponent implements OnInit {
+  counter: number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  decrease() {
+    if (this.counter - 1 <= 0) return;
+    this.counter--;
   }
-
+  increase() {
+    if (this.counter + 1 > 100) return;
+    this.counter++;
+  }
 }
